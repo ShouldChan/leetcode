e12c6380bddffeaa4a8984538344d0e44a9a1371
ShouldChan <ezanmydream@163.com>
2017-11-05 15:27:25 +0800

    longestsubstring_1527

--- LongestSubstring.java
+++ LongestSubstring.java
@@ -53,7 +53,19 @@ public class LongestSubstring {
 		return ans;
 	}
 
-
+	public int optimizeLenofLongestSubstring(String s) {
+		int n = s.length();
+		int ans = 0;
+		Map<Character, Integer> map = new HashMap<>();
+		for (int i = 0, j = 0; j < n; j++) {
+			if (map.containsKey(s.charAt(j))) {
+				i = Math.max(map.get(s.charAt(j)), i);
+			}
+			ans = Math.max(ans, j - i + 1);
+			map.put(s.charAt(j), j + 1);
+		}
+		return ans;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -62,6 +74,7 @@ public class LongestSubstring {
 		// System.out.println(result);
 		LongestSubstring ls = new LongestSubstring();
 		result = ls.lengthofLongestSubstring("abcabcbb");
+		result = ls.optimizeLenofLongestSubstring("pwwkew");
 		System.out.println(result);
 	}
 
