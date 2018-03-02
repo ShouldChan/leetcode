83ab5f626dd9201ec591d93b59302c33fb5e2cc8
ShouldChan <ezanmydream@163.com>
2018-03-02 13:19:01 +0800

    validParentheses_1318

--- ValidParentheses_20.java
+++ ValidParentheses_20.java
@@ -1,27 +1,34 @@
 package com.cxj.leetcode;
 
+import java.util.Stack;
+
 public class ValidParentheses_20 {
     public boolean isValid(String s) {
-        int i=0,j=s.length();
-        if(s.length()%2==1)
-            return false;
-        else{
-            while(i<=s.length()/2){
-                if(s.indexOf(i)==s.indexOf(j)){
-                    i++;
-                    j--;
-                }
-                else
+        if (s == null || s.length() == 0) return false;
+        if (s.length() % 2 != 0) return false;
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+                stack.push(s.charAt(i));
+            }else{
+                if(stack.empty())
                     return false;
-
+                char tmp=stack.peek();
+                if((tmp=='('&&s.charAt(i)==')')||(tmp=='['&&s.charAt(i)==']')||(tmp=='{'&&s.charAt(i)=='}')){
+                    stack.pop();
+                }
             }
+
         }
-        return true;
+        if(stack.empty())
+            return true;
+        else
+            return false;
     }
 
     public static void main(String[] args) {
         ValidParentheses_20 vp = new ValidParentheses_20();
-        boolean result = vp.isValid("({})");
+        boolean result = vp.isValid("(");
         System.out.println(result);
     }
 }
