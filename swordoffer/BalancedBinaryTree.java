701878286815d56290337bc753ab437618b906e3
ShouldChan <ezanmydream@163.com>
2018-04-09 17:11:41 +0800

    common_solution

--- swordoffer/BalancedBinaryTree.java
+++ swordoffer/BalancedBinaryTree.java
@@ -3,51 +3,53 @@ package com.cxj.swordoffer;
 /**
  * 题目描述：输入一棵二叉树，判断该二叉树是否是平衡二叉树。
  * 思路：
+ * 平衡二叉树 一颗二叉树任意节点的左右子树高度不超过1
  * 1）root为空，height=0, balanced=false;
- * 2）root->left==null&&root->right==null,height=1,balanced=true
- * 3）对左右子树递归，返回最高子树高度+1,balanced=true;高度差>1，false
+ * 2）左子树或者右子树不是平衡二叉树，返回false
+ * 3）如果左子树和右子树都是平衡二叉树了就获取左右的高度,balanced=true
  */
 public class BalancedBinaryTree {
-
-
+//    普通做法
+    //    逻辑判断
     public boolean IsBalanced_Solution(TreeNode root) {
-        return getDepth(root) != -1;
+        if (root == null)
+            return false;
+        if (!IsBalanced_Solution(root.left) || !IsBalanced_Solution(root.right))
+            return false;
+        int left = getDepth(root.left);
+        int right = getDepth(root.right);
+        if (Math.abs(left - right) <= 1) return true;
+
+        return false;
     }
 
-    private int getDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        int leftHeight = getDepth(root.left);
-        if (leftHeight == -1) {
-            return -1;
-        }
-        int rightHeight = getDepth(root.right);
-        if (rightHeight == -1) {
-            return -1;
-        }
-        return Math.abs(leftHeight - rightHeight) > 1 ? -1 : 1 + Math.max(leftHeight, rightHeight);
+    //    获取二叉树深度
+    public int getDepth(TreeNode node) {
+        if (node == null) return 0;
+        return 1 + Math.max(getDepth(node.left), getDepth(node.right));
     }
 
-//    public int getDepth(TreeNode node) {
-//        if (node == null)
-//            return 0;
-////        else if (node.left == null && node.right == null)
-////            return 1;
-//        else {
-//            int leftHeight = getDepth(node.left);
-//            int rightHeight = getDepth(node.right);
-//            System.out.println("left\t" + leftHeight);
-//            int height = (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
-//            return Math.abs(leftHeight - rightHeight) > 1 ? 0 : height;
-////            return height;
-//        }
+//    优化剪枝的思想，效率更高
+//    一边遍历一边判断是否平衡
+//    public boolean IsBalanced_Solution(TreeNode root) {
+//        return getDepth(root) != -1;
 //    }
 //
-//    public boolean judge_AVL(TreeNode node) {
-//        System.out.println(getDepth(node));
-//        return getDepth(node) != 0;
+//    private int getDepth(TreeNode root) {
+//        if (root == null) {
+//            return 0;
+//        }
+//        int leftHeight = getDepth(root.left);
+//        if (leftHeight == -1) {
+//            return -1;
+//        }
+//        int rightHeight = getDepth(root.right);
+//        if (rightHeight == -1) {
+//            return -1;
+//        }
+//        return Math.abs(leftHeight - rightHeight) > 1 ? -1 : 1 + Math.max(leftHeight, rightHeight);
 //    }
+
 
     public static void main(String[] args) {
         ReconstructBinaryTree rb = new ReconstructBinaryTree();
