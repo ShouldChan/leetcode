18ec61bda4ecdc84a3a2b9c5a9eb2f84150f6111
ShouldChan <ezanmydream@163.com>
2018-05-05 23:03:52 +0800

    singleNum_3

--- leetcode/SingleNumber_137.java
+++ leetcode/SingleNumber_137.java
@@ -1,9 +1,6 @@
 package com.cxj.leetcode;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 题目描述
@@ -11,6 +8,7 @@ import java.util.Set;
  */
 
 public class SingleNumber_137 {
+
     //    function1  将所有的元素存入set 然后对set里的元素乘以3最后减去数组中的元素 就还剩两个（出现一次的数） 然后除以2
     public static int singleNum_1(int[] array) {
         Set<Integer> set = new HashSet<>();
@@ -47,9 +45,36 @@ public class SingleNumber_137 {
         return key;
     }
 
+    //    function 3
+//    使用位运算
+    public static int singleNum_3(int[] array){
+        int[] bits=new int[32];
+        Arrays.fill(bits,0);
+        //bits save 所有数第j位的1的个数
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<32;j++){
+                bits[j] += ((array[i] >> j) & 1);
+            }
+        }
+//      当该数组所有元素都出现三次，那么每位的统计结果都可以被3整除，通过对该数组添加任何一个数字
+//        这个数的二进制的每一位加上去之后都不能被3整除。
+//        所以不能被三整除的那一位就是出现一次的数字的那一位，算出每一位上不能被3整除的二进制位之和那就是这个出现一次的数
+        int result=0;
+        for(int j=0;j<32;j++){
+            if(bits[j]%3!=0)
+                result+=(1<<j);
+        }
+        return result;
+
+    }
+
+
     public static void main(String[] args) {
         int[] array = {11, 11, 11, 3, 2, 2, 2, 6, 6, 6};
-        System.out.println(singleNum_1(array));
-        System.out.println(singleNum_2(array));
+//        System.out.println(singleNum_1(array));
+//        System.out.println(singleNum_2(array));
+        System.out.println(singleNum_3(array));
     }
+
+
 }
