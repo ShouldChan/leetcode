c07a909805744aed36c4280442faaef70d61bcf9
ShouldChan <ezanmydream@163.com>
2018-05-05 14:28:11 +0800

    treenode_1428

--- onlinetest/TreeNode.java
+++ onlinetest/TreeNode.java
@@ -4,17 +4,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TreeNode<T> {
-    public T t;
-    private TreeNode<T> parent;
-    public List<TreeNode<T>> nodelist;
 
-    public TreeNode(T type) {
-        t = type;
-        parent = null;
-        nodelist = new ArrayList<TreeNode<T>>();
+
+    private T cont;
+
+    private TreeNode<T> parent;
+    private List<TreeNode<T>> nodelist;
+
+    public TreeNode(T cont) {
+        this.cont = cont;
+        this.parent = null;
+        this.nodelist = new ArrayList<TreeNode<T>>();
+    }
+
+    public T getCont() {
+        return cont;
+    }
+
+    public void setCont(T cont) {
+        this.cont = cont;
     }
 
     public TreeNode<T> getParent() {
         return parent;
     }
+
+    public void setParent(TreeNode<T> parent) {
+        this.parent = parent;
+    }
+
+    public List<TreeNode<T>> getNodelist() {
+        return nodelist;
+    }
+
+    public void setNodelist(List<TreeNode<T>> nodelist) {
+        this.nodelist = nodelist;
+    }
+
 }
