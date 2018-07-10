d6bc4dd82e3c088a0a9d0fbe2acf8e7a90fd72d2
ShouldChan <ezanmydream@163.com>
2018-07-10 15:06:39 +0800

    balancedTree_edited

--- swordoffer/BalancedBinaryTree.java
+++ swordoffer/BalancedBinaryTree.java
@@ -9,7 +9,7 @@ package com.cxj.swordoffer;
  * 3）如果左子树和右子树都是平衡二叉树了就获取左右的高度,balanced=true
  */
 public class BalancedBinaryTree {
-//    普通做法
+    //    普通做法
     //    逻辑判断
     public boolean IsBalanced_Solution(TreeNode root) {
         if (root == null)
@@ -19,8 +19,8 @@ public class BalancedBinaryTree {
         int left = getDepth(root.left);
         int right = getDepth(root.right);
         if (Math.abs(left - right) <= 1) return true;
-
-        return false;
+        else
+            return false;
     }
 
     //    获取二叉树深度
@@ -50,7 +50,6 @@ public class BalancedBinaryTree {
 //        return Math.abs(leftHeight - rightHeight) > 1 ? -1 : 1 + Math.max(leftHeight, rightHeight);
 //    }
 
-
     public static void main(String[] args) {
         ReconstructBinaryTree rb = new ReconstructBinaryTree();
         /**
@@ -58,11 +57,12 @@ public class BalancedBinaryTree {
          *        2    3
          *      4    5   6
          *        7     8
+         *        左右子树的高度不超过1
          */
         int[] pre = {1, 2, 4, 7, 3, 5, 6, 8};
         int[] in = {4, 7, 2, 1, 5, 3, 8, 6};
         TreeNode node = rb.reConstructBinaryTree(pre, in);
-        System.out.println(node.left.val);
+//        System.out.println(node.left.val);
         BalancedBinaryTree avl = new BalancedBinaryTree();
         System.out.println(avl.getDepth(node));
         System.out.println(avl.IsBalanced_Solution(node));
