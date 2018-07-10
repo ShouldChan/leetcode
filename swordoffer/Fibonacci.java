43e04173f07b8b9aa6a3441c8bca5b5cab15e155
ShouldChan <ezanmydream@163.com>
2018-07-10 15:34:28 +0800

    fibonacci_dp

--- swordoffer/Fibonacci.java
+++ swordoffer/Fibonacci.java
@@ -12,10 +12,21 @@ public class Fibonacci {
 
     }
 
+    public int dp_Fibonacci(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         int n = 10;
         Fibonacci fb = new Fibonacci();
         int result = fb.recur_Fibonacci(n);
         System.out.println(result);
+        System.out.println(fb.dp_Fibonacci(n));
     }
 }
