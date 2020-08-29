55074a0cda93fa416d18785eaa2e151e0b474d08
ShouldChan <ezanmydream@163.com>
2020-08-29 23:00:06 +0800

    commit-2020-08-29 I visited the dentist in Xiaoshan Chinese Medicine Hospital in the early morning.

---
+++ JiebiSort/GetLeastNum.java
@@ -0,0 +1,61 @@
+package JiebiSort;
+
+import java.util.ArrayList;
+
+/**
+ * @author magicjay
+ * @version 1.0
+ * @date 2020/8/28
+ * @description 最小的k个数
+ * 分治法 堆排序
+ */
+public class GetLeastNum {
+    public ArrayList<Integer> GetLeastNumbers_Solution(int[] input, int k) {
+        ArrayList<Integer> ret = new ArrayList<>();
+        if (k > input.length || k <= 0) {
+            return ret;
+        }
+        findKthSmallest(input, k - 1);
+        for (int i = 0; i < k; i++) {
+            ret.add(input[i]);
+        }
+        return ret;
+    }
+
+    public void findKthSmallest(int[] nums, int k) {
+        int l = 0, h = nums.length - 1;
+        while (l < h) {
+            int j = partition(nums, l, h);
+            if (j == k) {
+                break;
+            }
+            if (j > k) {
+                h = j - 1;
+            } else {
+                l = j + 1;
+            }
+        }
+    }
+
+    private int partition(int[] nums, int l, int h) {
+        int p = nums[l];
+        int i = l, j = h + 1;
+        while (true) {
+            while (i != h && nums[++i] < p) ;
+            while (j != l && nums[--j] > p) ;
+            if (i > j) {
+                break;
+            }
+            swap(nums, i, j);
+        }
+        swap(nums, l, j);
+        return j;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+}
+
